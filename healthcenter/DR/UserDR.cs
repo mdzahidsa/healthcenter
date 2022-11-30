8e245db02ca312fe3bbe5799153d6470387a09dc
jeevan <jeevansai.makam@gmail.com>
2022-11-30 22:50:20 +0000

    added files

---
+++ healthcenter/DR/UserDR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataRepository.DRs.Admin
+{
+    public class UserDR
+    {
+
+        #region Public Methods
+        public UserDTO GetUsers()
+        {
+
+        }
+    }
+}
---
+++ healthcenter/DTO/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class userDTO
+{
+	public userDTO()
+	{
+	}
+}
