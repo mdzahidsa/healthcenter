e92aed28804c77064361f12b4118eed1582abbc7
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2022-12-15 22:56:26 +0000

    Added Test Feature

---
+++ Health/DR/DropDownDR.cs
@@ -0,0 +1,29 @@
+﻿using Health.DTO;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace Health.DR
+{
+    public class DropDownDR
+    {
+        public DropDownDTO GetLabTest(SqlConnection connection)
+        {
+            DropDownDTO DropDownDTO_ = new DropDownDTO();
+            List<LabTest> LabTest_ = new List<LabTest>();
+            SqlDataAdapter command = new SqlDataAdapter("select * from LabTest", connection);
+            connection.Open();
+            SqlDataReader rdr = command.SelectCommand.ExecuteReader();
+            while (rdr.Read())
+            {
+                LabTest _LabTest = new LabTest();
+                _LabTest.LabTestID = Convert.ToInt32(rdr["LabTestID"]);
+                _LabTest.TestName = rdr["TestName"].ToString();
+                LabTest_.Add(_LabTest);
+            }
+            DropDownDTO_.LabTestList = LabTest_;
+            return DropDownDTO_;
+        }
+    }
+}
